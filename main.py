a4057ffb7b73bd9a9b5788b46452e62874130436
ingo wolf <0358381@schoolsnet.act.edu.au>
2024-06-07 00:43:02 +0000

    a full comit

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 import os
 
-os.system("pip install docker")
+os.system("pip install docker flask")
 
-import docker, time
+import docker, time, webbrowser, threading
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 client = docker.from_env()
 
-client.containers.run(
-    "lscr.io/linuxserver/webtop:ubuntu-kde",
-    detach=True,
-    name="webdesktop",
-    security_opt=["seccomp:unconfined"],
-    environment=[
-        "PUID=1000",
-        "PGID=1000",
-        "TZ=Etc/UTC",
-        "SUBFOLDER=/",
-        "TITLE=WebDesktop",
-    ],
-    volumes=["/workspace/WebDesktop/files:/config"],
-    ports={"3000": "3000"},
-)
+client.images.pull("lscr.io/linuxserver/webtop:ubuntu-kde")
 
-input()
 
-client.containers.list()[0].stop()
-client.containers.list()[0].remove()
+def resetContainer():
+    try:
+        client.containers.list()[0].stop()
+        client.containers.list()[0].remove()
+    except:
+        pass
+
+    client.containers.run(
+        "lscr.io/linuxserver/webtop:ubuntu-kde",
+        detach=True,
+        name="webdesktop",
+        security_opt=["seccomp:unconfined"],
+        environment=[
+            "PUID=1000",
+            "PGID=1000",
+            "TZ=Etc/UTC",
+            "SUBFOLDER=/",
+            "TITLE=WebDesktop",
+        ],
+        volumes=["/workspace/WebDesktop/files:/config"],
+        ports={"3000": "3000"},
+    )
+
+def openPreview():
+    os.system("gp preview http://localhost:8000/embed")
+
+
+@app.route("/")
+def index():
+    return "home"
+
+if __name__ == "__main__":
+    thread = threading.Thread()
+    app.run(port=8000)
