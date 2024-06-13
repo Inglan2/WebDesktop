e8067a400a62803b50f391e3010727d10edba75c
ingo wolf <0358381@schoolsnet.act.edu.au>
2024-06-13 03:06:05 +0000

    Use textual for basic UI

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 import os
 
-os.system("pip install docker flask")
+os.system("clear")
 
-import docker, time, webbrowser, subprocess
+print("Installing...")
+
+os.system("pip install docker flask textual > /dev/null")
+
+import docker, time, webbrowser, subprocess, threading
 from flask import Flask, render_template
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Button, Label
 
 app = Flask(__name__)
+
+serverthread = threading.Thread(target=lambda: app.run(host="0.0.0.0", port=6969, debug=False, use_reloader=False))
+serverthread.daemon = True
+
+class WebDesktop(App):
+    CSS = """
+    Screen {
+        align: center middle;
+    }
+
+    * {
+        margin: 1;
+        align: center middle;
+        width: 69
+    }
+    """
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "open":
+            webbrowser.open("http://localhost:6969")
+        elif event.button.id == "stop":
+            exit()
+
+    def compose(self) -> ComposeResult:
+        yield Label("WebDesktop is running")
+        yield Button("Open", id="open", variant="success")
+        yield Button("Stop", id="stop", variant="error")
 
 client = docker.from_env()
 
@@ -39,25 +71,7 @@ def resetContainer():
 def index():
     return render_template("index.html")
 
-@app.route("/embed")
-def embed():
-    return """
-    <style>
-        * {
-            width: 100%;
-            height: 100%;
-            padding: 0;
-            margin: 0;
-            background-color: #1F1F1F;
-            color: white;
-            font-size: 30px;
-            outline: 0;
-            border: 0;
-        }
-    </style>
-    <a href="/" target="_blank">Drag to the tab bar</a>
-    """
-
 if __name__ == "__main__":
-    subprocess.Popen(["sleep 5 && gp preview http://localhost:8000/embed"], shell=True)
-    app.run(port=8000)
+    serverthread.start()
+    txapp = WebDesktop()
+    txapp.run()
